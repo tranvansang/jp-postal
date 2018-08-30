3ed1b1ad64be0ec07ffab20ffe324163e5083f1b
Tran Sang <tranvansangk41@gmail.com>
2018-08-30 12:12:03 +0900

    bound i+step by max

--- gen.js
+++ gen.js
@@ -6,6 +6,6 @@ const end = 80000
 const key = process.env.GOOGLE_MAPS_API_KEY
 for(let i = start; i < end; i += step){
   const first = i
-  const last = i + step
+  const last = Math.min(i + step, end)
   console.log(`env START_INDEX=${first} END_INDEX=${last} GOOGLE_MAPS_API_KEY=${key} yarn geo >log_${first}-${last}.log &`)
 }
