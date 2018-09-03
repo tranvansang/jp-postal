85d841f135fdf7b96eacbc5368f9a3bb4d243c6f
Tran Sang <tranvansangk41@gmail.com>
2018-09-03 18:33:27 +0900

    use dev version babel

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-import path from 'path';
-import fs from 'fs';
+"use strict";
 
-const postalRaw = fs.readFileSync(path.resolve(__dirname, 'postal.json'), 'utf8');
-export default JSON.parse(postalRaw);
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.postalGeo = exports.extendedPostalData = exports.default = void 0;
 
-const postalExtendedRaw = fs.readFileSync(path.resolve(__dirname, 'postal-extended.json'), 'utf8');
-export const extendedPostalData = JSON.parse(postalExtendedRaw);
+var _path = _interopRequireDefault(require("path"));
 
-const geoFilePath = path.resolve(__dirname, 'geo.json');
-export const postalGeo = {};
-if (fs.existsSync(geoFilePath)) {
-  const geoRaw = fs.readFileSync(geoFilePath, 'utf8');
-  for (const [key, val] of Object.entries(JSON.parse(geoRaw))) postalGeo[key] = val;
+var _fs = _interopRequireDefault(require("fs"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+var postalRaw = _fs.default.readFileSync(_path.default.resolve(__dirname, 'postal.json'), 'utf8');
+
+var _default = JSON.parse(postalRaw);
+
+exports.default = _default;
+
+var postalExtendedRaw = _fs.default.readFileSync(_path.default.resolve(__dirname, 'postal-extended.json'), 'utf8');
+
+var extendedPostalData = JSON.parse(postalExtendedRaw);
+exports.extendedPostalData = extendedPostalData;
+
+var geoFilePath = _path.default.resolve(__dirname, 'geo.json');
+
+var postalGeo = {};
+exports.postalGeo = postalGeo;
+
+if (_fs.default.existsSync(geoFilePath)) {
+  var geoRaw = _fs.default.readFileSync(geoFilePath, 'utf8');
+
+  var _arr = Object.entries(JSON.parse(geoRaw));
+
+  for (var _i = 0; _i < _arr.length; _i++) {
+    var _arr$_i = _slicedToArray(_arr[_i], 2),
+        key = _arr$_i[0],
+        val = _arr$_i[1];
+
+    postalGeo[key] = val;
+  }
 }
