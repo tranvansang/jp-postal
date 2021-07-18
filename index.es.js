b600da6cb7adc14836feb9e006f99959c9726112
Tran Sang <tr@nsang.me>
2021-07-18 12:22:59 +0900

    remove geo

--- index.es.js
+++ index.es.js
@@ -7,14 +7,6 @@ export default JSON.parse(postalRaw)
 // const postalExtendedRaw = fs.readFileSync(path.resolve(__dirname, 'postal-extended.json'), 'utf8')
 // export const extendedPostalData = JSON.parse(postalExtendedRaw)
 
-const geoFilePath = path.resolve(__dirname, 'geo.json')
-export const postalGeo = {}
-if (fs.existsSync(geoFilePath)) {
-  const geoRaw = fs.readFileSync(geoFilePath, 'utf8')
-  for (const [key, val] of Object.entries(JSON.parse(geoRaw)))
-    postalGeo[key] = val
-}
-
 // const kanaRaw = fs.readFileSync(path.resolve(__dirname, 'kana.json'), 'utf8')
 // export const kana = JSON.parse(kanaRaw)
 
